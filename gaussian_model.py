Fix logical errors - Wrong densification iteration start - Wrong normalization on 4D rotations

gaussian_model.py
@@ -139,7 +139,10 @@ class GaussianModel(nn.Module):
 
     @property
     def get_rotation(self):
-        return self.rotation_activation(self._rotation)
+        if self.gaussian_dim == 3:
+            return self.rotation_activation(self._rotation)
+        else:
+            return self.rotation_activation(self._rotation.view(-1, 4, 2)).view(-1, 8)
 
     @property
     def get_mean(self):

train.py
@@ -109,7 +109,7 @@ def training(config):
         with torch.no_grad():
 
             # Densification
-            if iteration > config.densification.densify_from_iter and iteration < config.densification.densify_until_iter:
+            if iteration >= config.densification.densify_from_iter and iteration < config.densification.densify_until_iter:
 
                 if iteration % config.densification.densification_interval == 0:
                     gaussians.densify_and_prune()