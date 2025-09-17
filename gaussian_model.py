6d281866b196dad7c4255bc97ff3bb0db4ade596
June <jooeun9199@gmail.com>
2025-09-17 20:09:09 +0000

    Fix pruning logics

--- gaussian_model.py
+++ gaussian_model.py
@@ -533,9 +533,9 @@ class GaussianModel(nn.Module):
         self.densify_and_split(grads)
 
         prune_mask = (self.get_opacity < min_opacity).squeeze() \
-            & (self.get_scaling.max(dim=1).values < min_scale) \
-            & (self.get_scaling.max(dim=1).values > max_scale) \
-            & ((self.get_mean > 1) | (self.get_mean < -1)).any(dim=1)
+            | (self.get_scaling.max(dim=1).values < min_scale) \
+            | (self.get_scaling.max(dim=1).values > max_scale) \
+            | ((self.get_mean > 1) | (self.get_mean < -1)).any(dim=1)
 
         self.prune_points(prune_mask)
 
